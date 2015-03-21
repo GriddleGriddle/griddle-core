0113cf937b657a2f2bead1fda28d50ccd535208e
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-03-21 00:34:08 -0400

    Add webpack hotloader

---
+++ js/index.js
@@ -0,0 +1,4 @@
+var Test = require('./views/test');
+var React = require('react');
+
+React.render(<Test />, document.getElementById('main'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './js/index'
+  ],
+  output: {
+    path: __dirname + '/build/',
+    filename: 'griddle.js',
+    publicPath: '/build/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
+    ]
+  }
+};
