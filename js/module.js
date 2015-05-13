ae89554c19d9a36d837f0fb7c0c8e1f68d2c2c33
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-05-13 01:17:45 -0400

    Make development / npm package build

---
+++ js/module.js
@@ -0,0 +1,5 @@
+
+module.exports = {}
+module.exports.LocalActions = require('./actions/local-action-creators');
+module.exports.DataStore = require('./stores/data-store');
+module.exports.LocalDataPlugin = require('./stores/local-data-plugin');
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,55 @@
 var webpack = require('webpack');
 
-module.exports = {
-  devtool: 'source-map',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './js/index'
-  ],
-  output: {
+var currentEnv = process.env['NODE_ENV'];
+
+var env = {
+  production: currentEnv === 'production',
+  development: currentEnv === 'development'
+};
+
+var entry = null;
+var plugins = [];
+var moduleLoaders = null;
+var output = {
     path: __dirname + '/build/',
     filename: 'griddle.js',
     publicPath: '/build/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
-  ],
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  module: {
+};
+
+if(env.production){
+  plugins.push(new webpack.optimize.DedupePlugin());
+  plugins.push(new webpack.optimize.UglifyJsPlugin({ output: {comments: false} }));
+  moduleLoaders = {
+    loaders: [
+      { test: /\.jsx?$/, loaders: ['babel'], exclude: /node_modules/ }
+    ]
+  };
+  output.libraryTarget = 'commonjs2';
+  entry = './js/module';
+} else {
+  plugins.push(new webpack.HotModuleReplacementPlugin()),
+  plugins.push(new webpack.NoErrorsPlugin())
+  moduleLoaders ={
     loaders: [
       { test: /\.jsx?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ }
     ]
-  }
+  };
+  entry = [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './js/index'
+    ]
+}
+
+var exports = {
+  devtool: 'source-map',
+  plugins: plugins,
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: output,
+  entry: entry,
+  module: moduleLoaders
 };
+
+module.exports = exports;
