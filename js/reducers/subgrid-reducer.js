75d8d5bd88bf23e6faf1d4ac75b7408b3f35d508
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-08-29 15:08:33 -0400

    Show child rows on click

--- js/reducers/subgrid-reducer.js
+++ js/reducers/subgrid-reducer.js
@@ -8,31 +8,32 @@ import extend from 'lodash.assign';
 
 export function AFTER_REDUCE(state, action, helpers) {
   const data = state.get('visibleData');
-  const factory = ((settings) => { return {...settings, depth : settings.depth +1, visible: false }});
-
-  const flattenedData = data.flatMap(FlatMapHelper, {settings: {parentId: null, depth: 0, expanded: false, visible: true}, factory });
+  const factory = ((settings, parent) => { console.log(parent.toJSON()); return {...settings, depth : settings.depth +1, visible: parent.get('expanded'), parentId: parent.get('id') }});
+  const flattenedData = data.flatMap(FlatMapHelper, { settings: {parentId: null, depth: 0, visible:  true}, factory });
 
   //TODO: This is currently kind of a duplicate operation.
   //      we will need to do something to mimic this functionality
   //      in the flatmap or somethign along those lines
   const columns = helpers.getDataColumns(state, data);
-
   return state
     .set('visibleData', helpers.getSortedColumns(flattenedData, columns))
 }
 
 export function GRIDDLE_ROW_EXPANDED(state, action, helpers) {
-  debugger;
-  return state;
+  const griddleKey = { action }
+  return state.setIn(
+    ['data',
+      state.get('data').findIndex(function(row) { return row.get('griddleKey') === griddleKey }),
+      'expanded'
+    ], true)
 }
 
 export function GRIDDLE_LOADED_DATA_AFTER(state, action, helpers) {
   const data = state.get('data');
-
   const columns =   helpers.getDataColumns(state, data);
   const newData = setRowProperties(data, { columns });
 
-  return state.set('data', data);
+  return state.set('data', newData);
 }
 
 export function setRowProperties(data, properties) {
