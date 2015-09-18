25bff4bfa562177b0ee1fcbeebe56e062b26a56c
Joel L <floptical.bob@gmail.com>
2015-09-17 22:53:37 -0400

    Second commit of initial infinite scroll logic.  Yep.

---
+++ js/helpers/position-helpers.js
@@ -0,0 +1,50 @@
+'use strict';
+
+export function getRenderedData(state) {
+  state = state;
+  return state.get('renderedData');
+}
+
+export function getPositionData(state) {
+  state = state;
+  return state.get('currentPosition').toJS();
+}
+
+export function shouldUpdateDrawnRows(action, state) {
+  let yScrollChangePosition = state.getIn(['currentPosition', 'yScrollChangePosition']);
+  let rowHeight = state.getIn(['currentPosition', 'rowHeight']);
+
+  return Math.abs(action.yScrollPosition - yScrollChangePosition) >= rowHeight;
+}
+
+export function updatePositionProperties(action, state, helpers, force) {
+  if (!action.force && !helpers.shouldUpdateDrawnRows(action, state)) {
+    return state; // Indicate that this shouldn't result in an emit.
+  }
+  let rowHeight = state.getIn(['currentPosition', 'rowHeight']);
+  let tableHeight = state.getIn(['currentPosition', 'tableHeight']);
+
+  let adjustedHeight = rowHeight;
+  let visibleRecordCount = Math.ceil(tableHeight / adjustedHeight);
+
+  let visibleDataLength = helpers.getVisibleData(state).count();
+
+  // Inspired by : http://jsfiddle.net/vjeux/KbWJ2/9/
+  let renderedStartDisplayIndex = Math.max(0, Math.floor(action.yScrollPosition / adjustedHeight) - visibleRecordCount * 0.25);
+  let renderedEndDisplayIndex = Math.min(renderedStartDisplayIndex + visibleRecordCount * 1.25, visibleDataLength - 1) + 1;
+
+  return state
+    .setIn(['currentPosition', 'renderedStartDisplayIndex'], renderedStartDisplayIndex)
+    .setIn(['currentPosition', 'renderedEndDisplayIndex'], renderedEndDisplayIndex)
+    .setIn(['currentPosition', 'visibleDataLength'], visibleDataLength)
+    .setIn(['currentPosition', 'yScrollChangePosition'], action.yScrollPosition)
+    .setIn(['currentPosition', 'xScrollChangePosition'], action.xScrollPosition);
+}
+
+export function updateRenderedData(state, helpers) {
+  let startDisplayIndex = state.getIn(['currentPosition', 'renderedStartDisplayIndex']); // -1
+  return state
+    .set('renderedData', helpers.getVisibleData(state)
+                          .skip(startDisplayIndex)
+                          .take(state.getIn(['currentPosition', 'renderedEndDisplayIndex']) - startDisplayIndex));
+}
---
+++ js/initialStates/position-state.js
@@ -0,0 +1,17 @@
+import Immutable from 'immutable';
+
+export default Immutable.fromJS({
+  currentPosition: {
+    xScrollChangePosition: 0,
+    yScrollChangePosition: 0,
+    renderedStartDisplayIndex: 0,
+    renderedEndDisplayIndex: 20,
+    visibleDataLength: 20,
+    tableHeight: 500,
+    tableWidth: 300,
+    rowHeight: 25,
+    defaultColumnWidth: 80,
+    infiniteScrollLoadTreshold: 50
+  },
+  renderedData: []
+});
