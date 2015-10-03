ddbf973a0b1e74fc47c5eb86ef1eb7fa38ce91d7
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-03 15:57:25 -0400

    Start AFTER_REDUCE tests

--- js/reducers/__tests__/localReducerTest.js
+++ js/reducers/__tests__/localReducerTest.js
@@ -1,29 +1,31 @@
 import Immutable from 'immutable';
 import * as Helpers from '../../helpers/local-helpers';
 import LocalReducer, {
-  GRIDDLE_LOADED_DATA
+  GRIDDLE_LOADED_DATA,
+  AFTER_REDUCE
 } from '../local-reducer';
 
 import extend from 'lodash.assign';
 
-describe('load data', () => {
-  const defaultData = [
-    {one: "one", two: "two", three: "three"},
-    {one: "un", two: "du", three: "trois"},
-    {one: "uno", two: "dos", three: "tres"},
-    {one: "ichi", two: "ni", three: "san"},
-  ];
-
-  const getMethod = (options) => {
-    if(!options.method) {
-      throw "Need a method to call"
-    }
-
-    const combined = extend({state: Immutable.fromJS({}), payload: {}, helpers: {}, method: null}, options);
-    const { state, payload, helpers, method } = combined;
-    return method.call(this, state, payload, helpers);
+const getMethod = (options) => {
+  if(!options.method) {
+    throw "Need a method to call"
   }
 
+  const combined = extend({state: Immutable.fromJS({}), payload: {}, helpers: {}, method: null}, options);
+  const { state, payload, helpers, method } = combined;
+  return method.call(this, state, payload, helpers);
+}
+
+const defaultData = [
+  {one: "one", two: "two", three: "three"},
+  {one: "un", two: "du", three: "trois"},
+  {one: "uno", two: "dos", three: "tres"},
+  {one: "ichi", two: "ni", three: "san"},
+];
+
+
+describe('load data', () => {
   const loadData = (options) => {
     return getMethod(extend(options, {method: GRIDDLE_LOADED_DATA}));
   }
@@ -63,7 +65,21 @@ describe('load data', () => {
 });
 
 describe('after reduce', () => {
+  const afterReduce = (options) => {
+    return getMethod(extend(options, {method: AFTER_REDUCE}));
+  }
+
+  const defaultHelpers = extend(Helpers, {
+    getDataSet: (state) => { return state; }
+  });
+
   it('sets visible data', () => {
-    
+    const visibleData = [{ one: "one", two: "two", three: "three" }];
+    const helpers = extend({}, defaultHelpers, {
+      getVisibleData: (state) => { return visibleData; },
+    });
+
+    const state = afterReduce({ helpers });
+    expect(state.get('visibleData')).toEqual(visibleData);
   })
 });
