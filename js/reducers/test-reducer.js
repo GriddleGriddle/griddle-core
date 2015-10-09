0275e3da9ff65a5d8295eec01ed6a5675888f46d
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-09 00:47:24 -0400

    Remove test code

--- js/reducers/test-reducer.js
+++
@@ -1,4 +0,0 @@
-import Immutable from 'immutable';
-
-export function GRIDDLE_LOADED_DATA_BEFORE(state, action, helpers) {console.log("HI FROM TEST"); return state; }
-export function GRIDDLE_LOADED_DATA_AFTER(state, action, helpers) {console.log("BYE FROM TEST"); return state; }
