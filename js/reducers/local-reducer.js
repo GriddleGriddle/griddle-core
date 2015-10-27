ef36cbcb364fff63dff5bfd9a722af0bace1eacd
Ryan Lanciaux <ryan.lanciaux@gmail.com>
2015-10-27 00:50:16 -0400

    Remove subgridReducer

--- js/reducers/local-reducer.js
+++ js/reducers/local-reducer.js
@@ -13,7 +13,6 @@ import Immutable from 'immutable';
 */
 export function GRIDDLE_LOADED_DATA(state, action, helpers) {
   const columns = action.data.length > 0 ? Object.keys(action.data[0]) : [];
-
   //set state's data to this
   const tempState = state
   .set('data', helpers.addKeyToRows(Immutable.fromJS(action.data)))
